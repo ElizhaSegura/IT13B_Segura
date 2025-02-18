21ee5a40814e771cbd9e4405355ddae62269af56
krystal cuarteros <charita121279@gmail.com>
2025-02-18 08:09:19 +0800

    arithmetic

--- Segura_IT13B_Activities/src/Prelim/Lab_act1_Arithmetics.java
+++ Segura_IT13B_Activities/src/Prelim/Lab_act1_Arithmetics.java
@@ -1,29 +1,24 @@
-
 package Prelim;
 
 import java.util.Scanner;
 
 public class Lab_act1_Arithmetics {
 
+    public static void main(String[] args) {
 
-     public static void main(String[] args){
-       
-        int x;
-        int y;
-        int z;
-       Scanner in = new Scanner (System.in);
-     System.out.println("Enter num1: ");
-        x= in.nextInt();
-           System.out.println("Enter num2: ");
-        y= in.nextInt();
-           System.out.println("Enter num3: ");
-        z= in.nextInt();
-           
-        
-        
-       System.out.println(x + " x " + y + " + " + z + " = " + (x * y + z));
-       System.out.println("(" +x + " - " + y + ")" + " % " + z + " = "+ ((x - y) % z)); 
-       System.out.println("(" + x + " + " + z + " + " + y + " / " + "3" + " = " + ((x + y + z) / 3));
-       System.out.println(x + " * " + z + " - " + "("+ y + " * " + y + ")" + " = " + (x * z - (y*y)));
-     }        
+        int x, y, z;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter num1: ");
+        x = in.nextInt();
+        System.out.println("Enter num2: ");
+        y = in.nextInt();
+        System.out.println("Enter num3: ");
+        z = in.nextInt();
+
+        System.out.println(x + " x " + y + " + " + z + " = " + (x * y + z));
+        System.out.println("(" + x + " - " + y + ")" + " % " + z + " = " + ((x - y) % z));
+        System.out.println("(" + x + " + " + z + " + " + y + " / " + "3" + " = " + ((x + y + z) / 3));
+        System.out.println(x + " * " + z + " - " + "(" + y + " * " + y + ")" + " = " + (x * z - (y * y)));
+    }
 }
